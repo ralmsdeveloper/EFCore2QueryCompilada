a7421e366d3e69177b783e98e10c6d32dec7cea3
Rafael Almeida <ralms@outlook.com>
2017-08-02 22:33:52 -0300

    Adicionar arquivos de projeto.

---
+++ EFCoreQueryCompilada/Program.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace EFCoreQueryCompilada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var db = new RalmsContext();
+            db.Database.EnsureCreated();
+            for (int i = 0; i < 20000; i++)
+            {
+                db.Clientes.Add(new Clientes
+                {
+                    RazSocial = $"RALMS DEVELOPER {i}",
+                    Fantasia = $"RALMS DEVELOPER {i}",
+                    Cidade = $"ITABAIANA {i}",
+                    Bairro = $"CENTRO {i}",
+                    CNPJ = $"035080{i.ToString("0000")}",
+                    Email = "ralms@ralms.net",
+                    Site = "www.ralms.net",
+                    Estado = "SE",
+                    Endereco = $"RUA TESTE. {i}",
+                    Numero = i,
+                    Observacoes = $"TESTE DE OBERVAÇÃO TESTE {i}"
+                });
+            }
+            db.SaveChanges();
+            //Ter o primeiro acesso
+            var cliente = db.Clientes.First(); 
+
+            TestarAplicacao(
+                testeSimples: (accountNumbers) =>
+                {
+                    using(var context = new RalmsContext())
+                        foreach (var id in accountNumbers)
+                        {
+                            var temp = context.Clientes.Single(c => c.Id == id);
+                        }
+                   
+                },
+                testeQueryCompilada: (accountNumbers) =>
+                {
+                    var query = EF.CompileQuery((RalmsContext banco, int id) =>
+                        db.Clientes.Single(c => c.Id == id));
+
+                    using (var context = new RalmsContext())
+                        foreach (var id in accountNumbers)
+                        {
+                            var temp = query(context, id);
+                        }
+                    
+                });
+
+            Console.ReadKey();
+        }
+
+        private static void TestarAplicacao(Action<int[]> testeSimples, Action<int[]> testeQueryCompilada)
+        {
+            var numeroTeste = NumeroDeTeste(200); 
+            var monitor = new Stopwatch();
+            monitor.Start();
+            testeSimples(numeroTeste);
+            monitor.Stop(); 
+            Console.WriteLine($"Teste Simples........: {monitor.ElapsedMilliseconds.ToString().PadLeft(4)}ms"); 
+            monitor.Restart();
+            testeQueryCompilada(numeroTeste);
+            monitor.Stop(); 
+            Console.WriteLine($"Teste Query Compilada: {monitor.ElapsedMilliseconds.ToString().PadLeft(4)}ms");
+        }
+
+        private static int[] NumeroDeTeste(int count)
+        {
+            var arrayContador = new int[count];
+            for (int i = 0; i < count; i++) 
+                arrayContador[i] = i + 1;
+       
+            return arrayContador;
+        }
+    }
+}
---
+++ EFCoreQueryCompilada/RalmsContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.ComponentModel.DataAnnotations;
+namespace EFCoreQueryCompilada
+{
+    public class RalmsContext : DbContext
+    {
+        public RalmsContext()
+        {}
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder opcao)
+        {
+            opcao.UseSqlite("Data Source=RALMS.DB;");
+        }
+
+        public DbSet<Clientes> Clientes { get; set; }
+    }
+     
+    public class Clientes
+    {
+        [Key]
+        public int Id { get; set; } 
+        public string RazSocial { get; set; }
+        public string Fantasia { get; set; }
+        public string CNPJ { get; set; } 
+        public string Endereco { get; set; }
+        public string Bairro { get; set; }
+        public int Numero { get; set; }
+        public string Estado { get; set; }
+        public string Cidade { get; set; } 
+        public string Email { get; set; }
+        public string Site { get; set; } 
+        public string Observacoes { get; set; } 
+
+    }
+}
+ 
